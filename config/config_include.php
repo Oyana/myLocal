818c33a20c753915af6d17ab131da9dda40c6c7d
Jiedara <m.villanove@laposte.net>
2016-06-12 11:16:41 +0200

    add a user friendly config file using json

--- config/config_include.php
+++ config/config_include.php
@@ -9,4 +9,8 @@ require_once("config_root.php");
 require_once("config_smarty.php");
 require_once(CLASS_DIR . "/Controller.php");
 require_once(CLASS_DIR . "/FrontController.php");
+
+if($configReader->getConfig()->myLocalUse = 'video'){
+    require_once(CLASS_DIR . "/VideoController.php");
+}
 ?>
--- config/defines.php
+++ config/defines.php
@@ -1,11 +1,16 @@
 <?php 
+
+include('config_reader.php');
+//init config reader
+$configReader = new ConfigReader();
+
 define("MYLOCAL_RELEASE", "0.4.0");
 define("DEV_MOD", true);
 define("COMPRESSED_FILES_MOD", true);
 define("CACHES_FILES_MOD", false);
 define("CHECK_COMPILATION", true);
 define("MAIN_FOLDER_NAME", "myLocal");
-define("TEMPLATE_NAME", "light2016");
+define("TEMPLATE_NAME", $configReader->getConfig()->template);
 define("UPDATE_SCAN", 1);
 define("REPO_SCAN", 1);
 $templateList = array( "head", "header", "index", "footer", "foot");
