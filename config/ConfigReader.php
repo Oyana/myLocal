6a9f22e99f6c7157ffc487360e69023ca2cc7ec1
Golga <golgarud@live.fr>
2016-06-22 09:36:03 +0200

    style: convert indent

--- config/ConfigReader.php
+++ config/ConfigReader.php
@@ -11,9 +11,9 @@
 class ConfigReader{
 
 	private $configJsonPath;
-    private $configJson;
-    private $mainFolderName;
-    private $templateName;
+	private $configJson;
+	private $mainFolderName;
+	private $templateName;
 
 	/**
 	 * __construct ConfigReader
@@ -23,96 +23,98 @@ class ConfigReader{
 	 */
 	public function __construct( $mainFolderName , $templateName, $configJsonPath = null)
 	{
-        $this->mainFolderName = $mainFolderName;
-        $this->templateName = $templateName;
-        $this->configRoot();
-        $this->configJsonPath = CUSTOM_FOLD . '/config.json';
-        $this->configJson = json_decode(file_get_contents($this->configJsonPath));
-        if(!empty($this->configJson->templateName)){
-            $this->templateName = $this->configJson->templateName;
-        }
-        $this->configMain();
+		$this->mainFolderName = $mainFolderName;
+		$this->templateName = $templateName;
+		$this->configRoot();
+		$this->configJsonPath = CUSTOM_FOLD . '/config.json';
+		$this->configJson = json_decode(file_get_contents($this->configJsonPath));
+		if(!empty($this->configJson->templateName)){
+			$this->templateName = $this->configJson->templateName;
+		}
+		$this->configMain();
 	}
 
-    public function getConfig($key = false){
-        if($key){
-            return $this->configJson->$key;
-        }else {
-            return $this->configJson;
-        }
-    }
+	public function getConfig($key = false){
+		if($key){
+			return $this->configJson->$key;
+		}else {
+			return $this->configJson;
+		}
+	}
 
-    //real path myLocal
-    public function rp( $path )
-    {
-        $out = array();
-        foreach(explode('/', $path) as $i=>$fold)
-        {
-            if ( $fold == '' || $fold == '.')
-            {
-                continue;
-            }
-            elseif ($fold == '..' && $i > 0 && end($out) != '..')
-            {
-                array_pop($out);
-            }
-            else
-            {
-                $out[]= $fold;
-            }
-        }
-        return ($path{0}=='/'?'/':'').join('/', $out);
-    }
+	//real path myLocal
+	public function rp( $path )
+	{
+		$out = array();
+		foreach(explode('/', $path) as $i=>$fold)
+		{
+			if ( $fold == '' || $fold == '.')
+			{
+				continue;
+			}
+			elseif ($fold == '..' && $i > 0 && end($out) != '..')
+			{
+				array_pop($out);
+			}
+			else
+			{
+				$out[]= $fold;
+			}
+		}
+		return ($path{0}=='/'?'/':'').join('/', $out);
+	}
 
-    //define config root
-    public function configRoot(){
-        $root = explode( basename($_SERVER['PHP_SELF']), $_SERVER["PHP_SELF"] );
+	//define config root
+	public function configRoot()
+	{
+		$root = explode( basename($_SERVER['PHP_SELF']), $_SERVER["PHP_SELF"] );
 
-        $InFold = explode( $this->mainFolderName, $root[0] );
-        // check path
+		$InFold = explode( $this->mainFolderName, $root[0] );
+		// check path
 
-        if ( !empty($this->mainFolderName) )
-        {
-            if ( isset($InFold[1]) )
-            {
-                chdir("..");
-            }
-            define("ROOT_DIR", $this->mainFolderName);
-            define("MAIN_FOLDER_NAME", $this->mainFolderName);
-            define("ROOT_LOCAL", "http://" . $_SERVER['HTTP_HOST'] . "/" );
-            define("ROOT_URL", "http://" . $_SERVER['HTTP_HOST'] . "/" . ROOT_DIR);
-        }
-        else{
-            define("ROOT_DIR",$root[0]);
-            define("ROOT_URL", "http://" . $_SERVER['HTTP_HOST'] . ROOT_DIR);
-        }
+		if ( !empty($this->mainFolderName) )
+		{
+			if ( isset($InFold[1]) )
+			{
+				chdir("..");
+			}
+			define("ROOT_DIR", $this->mainFolderName);
+			define("MAIN_FOLDER_NAME", $this->mainFolderName);
+			define("ROOT_LOCAL", "http://" . $_SERVER['HTTP_HOST'] . "/" );
+			define("ROOT_URL", "http://" . $_SERVER['HTTP_HOST'] . "/" . ROOT_DIR);
+		}
+		else{
+			define("ROOT_DIR",$root[0]);
+			define("ROOT_URL", "http://" . $_SERVER['HTTP_HOST'] . ROOT_DIR);
+		}
 
-        define("URL_DOM", "http://" . $_SERVER['HTTP_HOST'] . "/" );
+		define("URL_DOM", "http://" . $_SERVER['HTTP_HOST'] . "/" );
 
-        define("CONFIG_DIR", $this->rp(ROOT_DIR . "/config") );
-        define("CUSTOM_FOLD", $this->rp(ROOT_DIR . "/config-user") );
-        define("SHOOT_DIR", $this->rp( CUSTOM_FOLD. "/img") );
-    }
+		define("CONFIG_DIR", $this->rp(ROOT_DIR . "/config") );
+		define("CUSTOM_FOLD", $this->rp(ROOT_DIR . "/config-user") );
+		define("SHOOT_DIR", $this->rp( CUSTOM_FOLD. "/img") );
+	}
 
-    public function configMain() {
-        // class
-        define("CLASS_DIR", $this->rp( ROOT_DIR . "/Class" ) );
-        define("SMARTY_DIR", CLASS_DIR . "/Smarty");
-        define("SMARTY_SYSPLUGINS_DIR", SMARTY_DIR . "/sysplugins/");//END slashess used in smarty class
-        define("SMARTY_PLUGIN", SMARTY_DIR . "/plugins/");//END slashess used in smarty class
+	public function configMain()
+	{
+		// class
+		define("CLASS_DIR", $this->rp( ROOT_DIR . "/Class" ) );
+		define("SMARTY_DIR", CLASS_DIR . "/Smarty");
+		define("SMARTY_SYSPLUGINS_DIR", SMARTY_DIR . "/sysplugins/");//END slashess used in smarty class
+		define("SMARTY_PLUGIN", SMARTY_DIR . "/plugins/");//END slashess used in smarty class
 
-        // cache
-        define("CACHE_DIR", $this->rp( ROOT_DIR . "/cache" ) );
-        define("CACHE_SMARTY_DIR", CACHE_DIR . "/.smarty-cache");
-        define("CACHE_SMARTY_COMPILE", CACHE_DIR . "/.smarty-compile");
+		// cache
+		define("CACHE_DIR", $this->rp( ROOT_DIR . "/cache" ) );
+		define("CACHE_SMARTY_DIR", CACHE_DIR . "/.smarty-cache");
+		define("CACHE_SMARTY_COMPILE", CACHE_DIR . "/.smarty-compile");
 
-        // Templates
-        define("TEMPLATE_EXT", $this->rp(".tpl") );
-        define("TEMPLATE_DIR", $this->rp( ROOT_DIR . "/templates/" . $this->templateName) );
-        define("IMG_DIR", TEMPLATE_DIR . "/img");
-        define("JS_DIR", TEMPLATE_DIR . "/js");
-        define("CSS_DIR", TEMPLATE_DIR . "/css");
-    }
+		// Templates
+		define("TEMPLATE_EXT", $this->rp(".tpl") );
+		define("TEMPLATE_DIR", $this->rp( ROOT_DIR . "/templates/" . $this->templateName) );
+		define("IMG_DIR", TEMPLATE_DIR . "/img");
+		define("JS_DIR", TEMPLATE_DIR . "/js");
+		define("CSS_DIR", TEMPLATE_DIR . "/css");
+	}
 
 	/**
 	 * __destruct
