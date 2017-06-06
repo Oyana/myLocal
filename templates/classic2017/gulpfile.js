9ab36af8b99c19fcccda2a472d4b167cf450d70c
Golga <golgarud@live.fr>
2017-06-06 20:30:16 +0200

    CORE: remove compass  :weary:

--- templates/classic2017/gulpfile.js
+++ templates/classic2017/gulpfile.js
@@ -1,20 +1,17 @@
-var gulp =		require("gulp");
+var gulp =			require('gulp');
+var sass =			require('gulp-sass');
 var concat =		require("gulp-concat");
 var plumber =		require("gulp-plumber");
 var uglify =		require("gulp-uglify");
-var rename =		require("gulp-rename");
-var compass =		require('gulp-compass');
-
+var autoprefixer =	require('gulp-autoprefixer');
 
 var jsPath = "./js";
 var cssPath = "./css";
 var scssPath = "./scss";
 
-
 gulp.task('default', ['script-concat', 'watch']);
 
 gulp.task('script-concat', function(){
-
 	var files = [
 		jsPath+'/jquery_3_2_1.js',
 		jsPath+'/*.js',
@@ -27,17 +24,17 @@ gulp.task('script-concat', function(){
 		.pipe(gulp.dest(jsPath));
 });
 
-gulp.task('compass', function() {
-	gulp.src(cssPath + '/*.scss')
-		.pipe(compass({
-			config_file: './config.rb',
-			css: cssPath,
-			sass: scssPath
-		}))
-		.pipe(gulp.dest('app/assets/temp'));
+gulp.task('sass', function () {
+	return gulp.src(scssPath + '/**/*.scss')
+		.pipe(sass({
+			outputStyle: 'compressed',
+			includePaths: require('node-bourbon').includePaths
+		}).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(cssPath));
 });
 
 gulp.task('watch', function(){
-	gulp.watch(scssPath+"/**/*.scss", ['compass']);
+	gulp.watch(scssPath+"/**/*.scss", ['sass']);
 	gulp.watch([jsPath+"/**/*.js", "!"+jsPath+"/**/*min.js"], ['script-concat']);
 });
