c772618f2604e0b52f33e54266c144956fa6659a
Golga <golgarud@live.fr>
2017-07-16 12:45:55 +0200

    REFACTORE: remove bourbon & add gulp-oyana

--- templates/classic2017/gulpfile.js
+++ templates/classic2017/gulpfile.js
@@ -1,43 +1,9 @@
-var gulp =			require('gulp');
-var sass =			require('gulp-sass');
-var concat =		require("gulp-concat");
-var plumber =		require("gulp-plumber");
-var uglify =		require("gulp-uglify");
-var autoprefixer =	require('gulp-autoprefixer');
-
-var jsPath = "./js";
-var cssPath = "./css";
-var scssPath = "./scss";
-
-gulp.task('default', ['script-concat', 'watch']);
-
-gulp.task('script-concat', function(){
-	var files = [
-		jsPath+'/jquery_3_2_1.js',
-		jsPath+'/*.js',
-		'!'+jsPath+'/main.min.js'
-	];
-	gulp.src(files)
-		.pipe(plumber())
-		.pipe(concat('main.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(jsPath));
-});
-
-gulp.task('sass', function () {
-	return gulp.src(scssPath + '/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed',
-			includePaths: require('node-bourbon').includePaths
-		}).on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 5 versions', 'ie >= 10'],
-			cascade: true
-		}))
-		.pipe(gulp.dest(cssPath));
-});
-
-gulp.task('watch', function(){
-	gulp.watch(scssPath+"/**/*.scss", ['sass']);
-	gulp.watch([jsPath+"/**/*.js", "!"+jsPath+"/**/*min.js"], ['script-concat']);
-});
+var oyana = require('gulp-oyana');
+var options = {
+	"jsMinPath" : "./js",
+	"jsPath" : "./js",
+	"cssPath" : "./css",
+	"scssPath" : "./scss",
+	"outputStyle" : "compressed"
+}
+oyana( options );
